8d155090536bd9b1f0b5aaca3896f00150082ffc
zero <teclaizerai@gmail.com>
2022-12-11 16:19:54 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# IsbnDb Client
+
+## INSTALL
+
+```sh
+composer require opendaje/isbndb-client
+
+// add a PSR Http client
+
+composer require php-http/guzzle6-adapter
+```
---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
+
+    // register a single rule
+    //$rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_80,
+
+        PHPUnitSetList::PHPUNIT_91,
+    ]);
+};
